6c9b0ea3482a6031becc8aa9db0bef4ec5d1ffd5
Antonio Piga <antonio.piga93@gmail.com>
2024-01-16 21:39:45 +0100

    feat: first version

---
+++ packages/waitlist/create_thread.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+
+
+def main(args):
+    try:
+        counter = int(args['state'])
+    except:
+        counter = 0
+    try:
+        client = OpenAI(
+            organization=args.get('ORGANIZATION'),
+            api_key=args.get('API_KEY_ASSISTANT_API')
+        )
+
+        empty_thread = client.beta.threads.create()
+
+        return {
+            'body': {
+            'output': empty_thread.id,
+            'state': str(counter + 1)
+            }
+        }
+
+    except Exception as e:
+        raise f'An error occurred: {e}'
---
+++ packages/waitlist/notify.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+
+def main(args):
+    try:
+        counter = int(args['state'])
+    except:
+        counter = 0
+    input = args.get('input', '')
+
+    url = args.get('SLACK_URL')
+    headers = {'Content-type': 'application/json'}
+    payload = {'text': input}
+
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    if response.status_code == 200:
+        return {
+            'body': {
+            'output': 'ok',
+            'state': str(counter + 1)
+            }
+        }
+    else:
+        return {
+            'body': {
+            'output': 'ko',
+            'state': str(counter + 1)
+            }
+        }
